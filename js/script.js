6b54f16ddd027206d7a68adcee5816fa79091277
buraya.yuliya <burayayulya374@gmail.com>
2023-08-18 19:44:51 +0300

    implemented slider v1

--- js/script.js
+++ js/script.js
@@ -301,7 +301,50 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }, 4000);
   }
 
-  // fetch("http://localhost:3000/menu")
-  // .then(data => data.json())
-  // .then(res => console.log(res));
+  //Slider 1
+  let slideIndex = 1;
+  const slides = document.querySelectorAll('.offer__slide'),
+      prev = document.querySelector('.offer__slider-prev'),
+      next = document.querySelector('.offer__slider-next'),
+      total = document.querySelector('#total'),
+      current = document.querySelector('#current');
+
+  showSlides(slideIndex);
+
+  if (slides.length < 10) {
+      total.textContent = `0${slides.length}`;
+  } else {
+      total.textContent = slides.length;
+  }
+
+  function showSlides(n) {
+      if (n > slides.length) {
+          slideIndex = 1;
+      }
+      if (n < 1) {
+          slideIndex = slides.length;
+      }
+
+      slides.forEach((item) => item.style.display = 'none');
+
+      slides[slideIndex - 1].style.display = 'block'; // Как ваша самостоятельная работа - переписать на использование классов show/hide
+      
+      if (slides.length < 10) {
+          current.textContent =  `0${slideIndex}`;
+      } else {
+          current.textContent =  slideIndex;
+      }
+  }
+
+  function plusSlides (n) {
+      showSlides(slideIndex += n);
+  }
+
+  prev.addEventListener('click', function(){
+      plusSlides(-1);
+  });
+
+  next.addEventListener('click', function(){
+      plusSlides(1);
+  });
 });
